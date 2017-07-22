d310cbe196197dad9b36f0d71fc3eea7983ecf4d
EmielBarten <emielbarten@gmail.com>
2017-07-22 20:23:46 +0200

    Merge branch 'Emiel_Branch' into NewStrategyClass
    # Conflicts:
    #	DataFrameManipulation.py
    #	DataFrameManipulation.pyc

--- TestFile_Valerie.py
+++ TestFile_Valerie.py
@@ -23,4 +23,16 @@ vs_StartDate, vs_EndDate = m_Dfm.f_FindCommonDates([df_DBC, df_IWM])
 
 vi_Months = m_Dfm.f_CountTimePeriod('2012-01-01', '2012-07-08', 'monthly')
 
+<<<<<<< HEAD
 na_Dates = m_Dfm.f_ListTimePeriod(df_DBC['date_time'], 'monthly')
+=======
+na_Dates = m_Dfm.f_ListTimePeriod(df_DBC['date_time'], 'monthly')
+
+test1 = m_Dfm.f_CutDates(df_DBC)
+test2 = m_Dfm.f_CutDates([df_DBC])
+test3 = m_Dfm.f_CutDates([df_DBC, df_IWM])
+
+test4 = m_Dfm.f_CutDates(df_DBC, vs_StartDate, vs_EndDate)
+test5 = m_Dfm.f_CutDates([df_DBC], vs_StartDate, vs_EndDate)
+test6 = m_Dfm.f_CutDates([df_DBC, df_IWM], vs_StartDate, vs_EndDate)
+>>>>>>> Emiel_Branch
